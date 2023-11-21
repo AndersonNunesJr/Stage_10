8c6b46e4b9679586da43e1308957cfaf9e028715
Anderson Nunes de Paula Junior <andersonnunes2015@gmail.com>
2023-11-21 10:32:20 -0300

    Correção de URL : Atualização da Api.

--- RocketMovies/src/pages/Profile/index.jsx
+++ RocketMovies/src/pages/Profile/index.jsx
@@ -18,7 +18,7 @@ export function Profile() {
   const [passwordNew, setPasswordNew] = useState();
 
   const avatarUrl = user.avatar
-    ? `${api.defaults.baseURL}files/${user.avatar}`
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
     : avatarPlaceholder;
 
   const [avatar, setAvatar] = useState(avatarUrl);
