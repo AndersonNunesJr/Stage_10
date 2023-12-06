b9894dbc992178c26a012e7c4773187e077786fe
Anderson Nunes de Paula Junior <andersonnunes2015@gmail.com>
2023-12-06 13:07:51 -0300

    Update notes.routes.js

--- Rocketseat-modulo-10/src/routes/notes.routes.js
+++ Rocketseat-modulo-10/src/routes/notes.routes.js
@@ -8,7 +8,7 @@ const notesController =  new NotesController();
 
 notesRoutes.get("/",notesController.index);
 notesRoutes.post("/:user_id",notesController.create);
-notesRoutes.get("/:id",notesController.show);
+notesRoutes.get("/:user_id",notesController.show);
 notesRoutes.delete("/:id",notesController.delete);
 
 module.exports = notesRoutes;
