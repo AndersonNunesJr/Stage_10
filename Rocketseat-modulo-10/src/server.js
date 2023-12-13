d35f1a78fe4e86a22d70e8294ed08e29ade26d1a
Anderson Nunes de Paula Junior <andersonnunes2015@gmail.com>
2023-12-13 12:31:24 -0300

    .

--- Rocketseat-modulo-10/src/server.js
+++ Rocketseat-modulo-10/src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config")
 
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
@@ -36,7 +37,7 @@ app.get("/", (req, res) => {
   res.status(200).send("MEU SERVIDOR");
 });
 
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT || 3333;
 app.listen(PORT, () =>
   console.log(`Server 
 listening on port ${PORT}`)
